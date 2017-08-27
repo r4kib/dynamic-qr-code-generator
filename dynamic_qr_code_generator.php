3d2fb4da34b0baf26c742e75b8aaf9487e8511d0
rakib <rakib@previewtechs.com>
2017-08-27 15:39:05 +0600

    initial commit

---
+++ dynamic_qr_code_generator.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Plugin Name: Dyanomic QR Code Generator WordPress
+ * Plugin URI: https://rakibh.com/
+ * Description: Generate QR code for every page, post or whole Wordpress site. Share Smartly.
+ * Version: 0.0.1
+ * Author: codemier
+ * Author URI: https://rakibh.com/
+ * License: GPL2
+ */
+?>
+<?php
+//Adding action and filters
+add_action( 'add_meta_boxes', 'cm_qrcode_add_metabox' );
+add_action( 'wp_dashboard_setup', 'cm_qrcode_dashboard' );
+if(function_exists('cm_qrcode')==false){
+    function cm_qrcode(){
+        
+    }
+}
+
+if(function_exists('cm_qrcode_add_metabox')==false){
+    function cm_qrcode_add_metabox(){
+        $screens=array('post','page');
+        foreach($screens as $screen){
+            add_meta_box('cm_qrcode', 'QR code for the permalink', 'cm_qrcode_html', $screen,'side','high');
+        }    
+    }
+}
+
+function cm_qrcode_html(){
+    $link=urlencode(get_permalink());
+    $qr_img="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chld=H|4&chl=".$link;
+     $qr_img_l="https://chart.googleapis.com/chart?cht=qr&chs=540x540&chld=H|4&chl=".$link;
+    $html="<a href='".$qr_img_l ."' target='_blank'>";
+    $html .= "<img src='". $qr_img . "' Title='Click here to get larger version'";
+    $html .="</a>";
+    //$html .="<p> Click on Picture to download larger version</p>";
+    echo $html;
+
+}
+
+if(function_exists('cm_qrcode_dashboard')==false){
+    function cm_qrcode_dashboard(){
+    add_meta_box('cm_qrcode', 'QR code for website link', 'cm_qrcode_dashboard_html', 'dashboard','side','high');
+    }
+}
+function cm_qrcode_dashboard_html(){
+    $link=urlencode(get_site_url());
+    $qr_img="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=H|4&chl=".$link;
+     $qr_img_l="https://chart.googleapis.com/chart?cht=qr&chs=540x540&chld=H|4&chl=".$link;
+    $html="<a href='".$qr_img_l ."' target='_blank'>";
+    $html .= "<img src='". $qr_img . "' Title='Click here to get larger version'";
+    $html .="</a>";
+    $html .="<p> Click on Picture to download larger version</p>";
+    echo $html;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Dynamic QR Code Generator===
+Contributors: r4kib.hbo
+Donate link: http://www.codemire.com/
+Tags: QR code, 2D code, QR
+Requires at least: 3.0.1
+Tested up to: 4.2
+Stable tag: 0.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Generate QR code for every page, post or whole Wordpress site. Share Smartly.
+
+== Description ==
+
+(Quick Response code) A two-dimensional bar code that is widely used to cause a Web page to download into the user's smartphone when scanned with a mobile tagging app. 'Dyanomic QR Code Generator by codemire' plugin generate qr code for each and every page, post and site url . Its easy to use from your sidebars. No setting up needed just plug and play.
+
+= Key Feratures: =
+
+* Easy install
+* Very easy to configure.
+
+= Rate us/ Feedback: =
+
+Please take the time to let us and others know about your experiences by leaving a review, so that we can improve the plugin for you and other users.
+
+= Need More? =
+Please contact us if you need custom features or any other things.
+
+== Installation ==
+
+
+1. Upload `cm_qrcode.php` to the `/wp-content/plugins/cm_qrcode` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Enjoy the power of QR codes.
+
+== Frequently Asked Questions ==
+
+= Is this plugin 100% free? =
+Yes! and always will be.
+
+=Is this Ad-free?=
+Yes. 100% Ad-free.
+
+= Can I use it commercialy? =
+Yes! You are always free to use coomercially.
+
+
+
+== Screenshots ==
+
+1. QR code for Site URL
+2. QR code on Sidebar of Page Editor
+3. QR code on Sidebar of Post Editor
+
+== Changelog ==
+
+
+
+= 0.0.1 =
+Minor fixes in functionality
