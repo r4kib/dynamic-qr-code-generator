89a1d4b02108e6cb77c761561b07dab46a1b6b47
rakib <rakib@previewtechs.com>
2017-09-12 16:38:51 +0600

    Finalized Version 0.0.5

--- dynamic_qr_code_generator.php
+++ dynamic_qr_code_generator.php
@@ -16,9 +16,15 @@ public $plugin_name ="dynamic_qr_code_generator";
 public $txt_domain ="dynamic_qr_code_generator";
     public function  __construct()
     {
+        add_action('init',array($this,"register_script"));
         add_action('add_meta_boxes',array($this,"add_admin_metabox"));
         add_action('admin_enqueue_scripts',array($this,"load_admin_script"));
+        add_shortcode( 'dqr_code', array($this,"shortcode_dqr_code") );
 
+    }
+    public function register_script()
+    {
+        wp_register_script( 'rkb-dynamic-qr-code', plugins_url( $this->plugin_name .'/js/qrcode.min.js' , dirname(__FILE__) ), array('jquery'), '1.0', true);
     } 
     public function add_admin_metabox()
     {
@@ -32,15 +38,15 @@ public $txt_domain ="dynamic_qr_code_generator";
     {
         $post_id = $_REQUEST['post'];
         $url=get_permalink();
-        $html = '<div id="qrcode"><span>Shortcode: [dqr_code id="'.$post_id.'"]</span></div>';
+        $html = '<div id="qrcode"><span>Shortcode: [dqr_code post_id="'.$post_id.'"]</span></div>';
         $html .= $this->get_qrcode_js($url,200,"qrcode");
         echo $html;
     }
     public function load_admin_script()
     {
-        wp_enqueue_script( 'rkb-dynamic-qr-code', plugins_url( $this->plugin_name .'/js/qrcode.min.js' , dirname(__FILE__) ) );
+        wp_enqueue_script('rkb-dynamic-qr-code');
     }
-    public function get_qrcode_js($url, $size=null, $js_id=null)
+    public function get_qrcode_js($url, $size=null, $js_id=null,$color= null,$bgcolor= null)
     {
         if(null === $js_id){
             $js_id = "qrcode";
@@ -48,15 +54,56 @@ public $txt_domain ="dynamic_qr_code_generator";
         if(null === $size){
             $size = 100;
         }
+        if(null === $color){
+            $color = '#000000';
+        }
+        if(null === $bgcolor){
+            $bgcolor = '#ffffff';
+        }
         $script_format='<script type="text/javascript">
-        var qrcode = new QRCode(document.getElementById("%s"), {
-            width : %d,
-            height : %d
+        jQuery( document ).ready(function() {
+            var qrcode = new QRCode(document.getElementById("%s"), {
+                width : %d,
+                height : %d,
+                colorDark: "%s",
+                colorLight: "%s",
+                correctLevel : QRCode.CorrectLevel.H
+            });
+            qrcode.makeCode("%s");
         });
-        qrcode.makeCode("%s");
         </script>';
         
-       return sprintf($script_format,$js_id,$size,$size,$url);
+       return sprintf($script_format,$js_id,$size,$size,$color,$bgcolor,$url);
+    }
+    public function shortcode_dqr_code($atts)
+    {
+        $args = shortcode_atts( array(
+            'post_id' => '0',
+            'url' => '',
+            'size' =>'200',
+            'color' =>'#000000',
+            'bgcolor'=> '#ffffff'
+        ), $atts );
+
+        if($args['url']==''){
+            if($args['post_id']=="0"){
+                //no url or post id provided in shortcode return current page permalink
+                $url= get_permalink();
+                $js_id="qrcode-current";
+            }else{
+                $url= get_permalink($args['post_id']);
+                $js_id="qrcode-".$args['post_id'];
+                
+            }
+            
+        }else{
+            $url = $args['url'];
+            $js_id="qrcode-".substr(md5($args['url']), 0, 8);
+        }
+        wp_enqueue_script('rkb-dynamic-qr-code');
+        $html = '<span id="'.$js_id.'"></span>';
+        $html .= $this->get_qrcode_js($url,$args['size'],$js_id,$args['color'],$args['bgcolor']);
+        return $html;
     }
  }
  $rkb_dynamic_qr_code= new rkbDynamicQrCode();
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Dynamic QR Code Generator===
 Contributors: r4kib.hbo
-Donate link: http://www.codemire.com/
+Donate link: http://www.rakibh.com/
 Tags: QR code, 2D code, QR
 Requires at least: 3.0.1
-Tested up to: 4.2
-Stable tag: 0.0.1
+Tested up to: 4.8.1
+Stable tag: 0.0.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -12,12 +12,22 @@ Generate QR code for every page, post or whole Wordpress site. Share Smartly.
 
 == Description ==
 
-(Quick Response code) A two-dimensional bar code that is widely used to cause a Web page to download into the user's smartphone when scanned with a mobile tagging app. 'Dyanomic QR Code Generator by codemire' plugin generate qr code for each and every page, post and site url . Its easy to use from your sidebars. No setting up needed just plug and play.
+(Quick Response code) A two-dimensional bar code that is widely used to cause a Web page to download into the user's smartphone when scanned with a mobile tagging app. 'Dyanomic QR Code Generator' plugin generate qr code for each and every page, post and site url . Its easy to use from your sidebars. No setting up needed just plug and play.
+
+=Shortcode=
+You can use [dqr_code] shortcode to display QR code in frontend.
+Useage Example-
+*[dqr_code] - shows QR code for permalink of current post, page or custom post type.
+*[dqr_code url="http://example.com"] - shows QR code for "http://example.com".
+*[dqr_code post_id="99"] - shows QR code for permalink of post ID "99". (supports post,page or custom post type).
+*[dqr_code url="http://example.com"] - shows QR code for "http://example.com" and the 'post_id' parameter will be denied.
+*[dqr_code url="http://example.com" size="300" color="#000000" bgcolor="#ffffff"] - shows QR code for "http://example.com". Where the QR code will be 300px X300px size and will have "#000000" as color and "#ffffff" as background color. Defaults: Size= 200, Color="#000000", Background Color="#ffffff"; 
 
 = Key Feratures: =
 
 * Easy install
 * Very easy to configure.
+*Easy to use shortcor
 
 = Rate us/ Feedback: =
 
@@ -54,7 +64,9 @@ Yes! You are always free to use coomercially.
 
 == Changelog ==
 
-
+=0.0.5=
+Move to JS for being future proof.
+Added shortcode [dqr_code]
 
 = 0.0.1 =
 Minor fixes in functionality
